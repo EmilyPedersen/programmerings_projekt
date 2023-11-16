3a757b4483589884db69c9e2a8bfcbf25cee3c93
Olivia Jespersen <oliviajespersen@Olivias-MacBook-Pro.local>
2023-11-16 13:26:30 +0100

    Fixed the make_board function

--- board.py
+++ board.py
@@ -1,13 +1,15 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Board:
     black: list[int]
     white: list[int]
     white_plays: bool
 
+
 def make_board() -> Board:
     """Make a new board where the pieces are at their starting points. 
     """
-    Board(range(1,13), range(14,26), True)
+    return Board(list(range(1, 13)), list(range(14, 26)), True)
 
