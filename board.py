5ca00a3f484051c1401d4391a56fb7bd5cc5e558
EmilyPedersen <emipe23@student.sdu.dk>
2023-11-16 13:17:45 +0100

    Added the make_board function

--- board.py
+++ board.py
@@ -6,5 +6,8 @@ class Board:
     white: list[int]
     white_plays: bool
 
-
+def make_board() -> Board:
+    """Make a new board where the pieces are at their starting points. 
+    """
+    Board(range(1,13), range(14,26), True)
 
