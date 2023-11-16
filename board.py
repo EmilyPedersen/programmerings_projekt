563206e8e4dcb96dffbbed9e2a22c9c5b59cf524
EmilyPedersen <emipe23@student.sdu.dk>
2023-11-16 13:46:47 +0100

    Added the functions for white_plays, black and white

--- board.py
+++ board.py
@@ -10,6 +10,32 @@ class Board:
 
 def make_board() -> Board:
     """Make a new board where the pieces are at their starting points. 
+    >>> make_board()
+    Board(black=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], white=[14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25], white_plays=True)
     """
     return Board(list(range(1, 13)), list(range(14, 26)), True)
+
+
+def white_plays(b: Board) -> bool:
+    """Return True if it is whites turn.
+    >>> white_plays()
+    True
+    >>> white_plays()
+    False
+    """
+    return b.white_plays
+
+
+def black(b: Board) -> list[int]:
+    """Return a list that contains the indexes for all the points where there are black pieces in the given board.
+    >>> 
+    """
+    return b.black
+
+def white(b: Board) -> list[int]:
+    """Return a list that contains the indexes for all the points where there are white pieces in the given board.
+    >>> 
+    """
+    return b.white
+
 
