e6a7cd4410257b0c1e05341416eecbe4d24beaae
Olivia Jespersen <oliviajespersen@Olivias-MacBook-Pro.local>
2023-11-16 12:35:29 +0100

    Added the make_move function

--- move.py
+++ move.py
@@ -4,3 +4,8 @@ from dataclasses import dataclass
 class Move:
 	source: int
 	target: int
+	
+def make_move(scr: int, trg: int) -> Move:
+    """Return a move between the two indicies 
+    >>> make_move()"""
+    return Move(scr, trg)
