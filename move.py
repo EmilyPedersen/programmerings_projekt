882a3bc2f3d704e1990b243661d970a07571ffff
Olivia Jespersen <oliviajespersen@Olivias-MacBook-Pro.local>
2023-11-16 12:47:02 +0100

    Added the source

--- move.py
+++ move.py
@@ -1,13 +1,23 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Move:
 	source: int
 	target: int
-	
+
+
 def make_move(scr: int, trg: int) -> Move:
     """Return a move between the two indicies. 
     >>> make_move(13, 8)
     Move(source=13, target=8)
     """
     return Move(scr, trg)
+
+
+def source(m: Move) -> int:
+    """Return the source point
+    >>> source(make_move(13, 8))
+    13
+    """
+    return m.source
