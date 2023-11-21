ac71b34987e273f535b6d0d480b781c92a4e2b5c
Marcus Søndergaard <marcusodense@gmail.com>
2023-11-21 12:08:36 +0100

    Fixed test

--- test.py
+++ test.py
@@ -12,8 +12,9 @@ doctest.testmod(move_module)
 
 
 # Testing the game
-def move_list(moves: list[tuple[int, int]]) -> list[Move]:
-    return [make_move(source, target) for (source, target) in moves]
+def are_legal_moves(b: Board, moves: list[tuple[int, int]]) -> bool:
+    moves = [make_move(source, target) for (source, target) in moves]
+    return sorted(legal_moves(b), key=source) == sorted(moves, key=source)
 
 
 b = make_board()
@@ -22,7 +23,7 @@ b = make_board()
 assert white_plays(b)
 assert black(b) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 assert white(b) == [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
-assert legal_moves(b) == move_list([(17, 13), (18, 13), (19, 13)])
+assert are_legal_moves(b, [(17, 13), (18, 13), (19, 13)])
 assert not is_game_over(b)
 
 move(make_move(18, 13), b)
@@ -30,7 +31,7 @@ move(make_move(18, 13), b)
 assert not white_plays(b)
 assert black(b) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 assert white(b) == [14, 15, 16, 17, 19, 20, 21, 22, 23, 24, 25, 13]
-assert legal_moves(b) == move_list([(8, 18)])
+assert are_legal_moves(b, [(8, 18)])
 assert not is_game_over(b)
 
 move(make_move(8, 18), b)
@@ -38,7 +39,7 @@ move(make_move(8, 18), b)
 assert white_plays(b)
 assert black(b) == [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 18]
 assert white(b) == [14, 15, 16, 17, 19, 20, 21, 22, 23, 24, 25]
-assert legal_moves(b) == move_list([(17, 13), (19, 13), (23, 13)])
+assert are_legal_moves(b, [(17, 13), (19, 13), (23, 13)])
 assert not is_game_over(b)
 
 move(make_move(23, 13), b)
@@ -46,7 +47,7 @@ move(make_move(23, 13), b)
 assert not white_plays(b)
 assert black(b) == [1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12]
 assert white(b) == [14, 15, 16, 17, 19, 20, 21, 22, 24, 25, 13]
-assert legal_moves(b) == move_list([(3, 8)])
+assert are_legal_moves(b, [(3, 8), (11, 23)])
 assert not is_game_over(b)
 
 move(make_move(3, 8), b)
@@ -54,7 +55,7 @@ move(make_move(3, 8), b)
 assert white_plays(b)
 assert black(b) == [1, 2, 4, 5, 6, 7, 9, 10, 11, 12, 8]
 assert white(b) == [14, 15, 16, 17, 19, 20, 21, 22, 24, 25, 13]
-assert legal_moves(b) == move_list([(13, 3), (15, 3)])
+assert are_legal_moves(b, [(13, 3), (15, 3)])
 assert not is_game_over(b)
 
 move(make_move(15, 3), b)
@@ -62,5 +63,5 @@ move(make_move(15, 3), b)
 assert not white_plays(b)
 assert black(b) == [1, 2, 4, 5, 6, 7, 10, 11, 12, 8]
 assert white(b) == [14, 16, 17, 19, 20, 21, 22, 24, 25, 13, 3]
-assert legal_moves(b) == move_list([(14, 9), (20, 15)])
+assert are_legal_moves(b, [(4, 9), (5, 9), (8, 18), (10, 15), (11, 23)])
 assert not is_game_over(b)
