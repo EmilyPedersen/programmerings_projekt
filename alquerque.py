bca1200874fd8db0dce61c5016acdcf7bf84be3c
Marcus Søndergaard <marcusodense@gmail.com>
2023-11-27 10:27:14 +0100

    Wrote the prose

--- alquerque.py
+++ alquerque.py
@@ -1,17 +1,50 @@
 # Hello! Welcome to Alquerque!
-# Before we start... 
-    #Should the computer play white (y/n)?
+# Before we start...
+    # Should the computer play white (y/n)?
         # Please answer y or n.
     # Should the computer play black (y/n)?
         # Please answer y or n.
     # How smart should the computer be (1-7)?
         # Please enter a number in the range 1-7.
-# Ready! 
+# Ready!
     # The game will now start!
 # Game over!
     # Black has won!
     # White has won!
     # The game is a draw...
+
+print("""
+It's time to get quirky, let's play some Alquerque!
+
+But just before we get started, let's get the game charted...
+You can hear the crow cheer, but first let's get a couple things clear...
+Get your brain in high-gear, but first let's get the rules clear...
+
+Will you stand in the limelight or will you let the AI be white? (y/n)
+This is not the time to debate human rights,
+so please just decide if the AI should play white? (y/n)
+    Please just type y or n, it's all that we need.
+    You know who'd be proud of you? Frances Shand Kydd.
+
+If you need to hit the sack, will you just let the AI play black? (y/n)
+If you need to fight off an arson attack, just let the AI play black? (y/n)
+    I really am feeling the rhythm and blues,
+    a simple y or n, that's all you have to choose.
+
+We crafted this software, it's a true piece of art,
+now decide, should the AI be dumb or smart? (1-7)
+    With that attitude you won't come into heaven,
+    so please just pick a number between 1 and 7.
+
+It's time to get started, find your laminar flow. Let's hope your hands are steady. Ready, set, go!
+
+You fought with all your might and the game is now done,
+so it's a pleasure to announce that ... won!
+
+This game was as complex as the United States national law,
+but in the end 'twas for nothing, it ended in a draw.
+""")
+
 
 print("""
 
