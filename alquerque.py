9ebfa35d783dca289d8bc2fea0bdeaedc22f272a
Olivia Jespersen <oliviajespersen@Olivias-MacBook-Pro.local>
2023-11-27 09:43:16 +0100

    Added outline

--- alquerque.py
+++ alquerque.py
@@ -1,1 +1,28 @@
+# Hello! Welcome to Alquerque!
+# Before we start... 
+    #Should the computer play white (y/n)?
+        # Plase answer y or n.
+    # Should the computer play black (y/n)?
+        # Plase answer y or n.
+    # How smart should the computer be (1-7)?
+        # Please enter a number in the range 1-7.
+# Ready! 
+    # The game will now start!
+# Game over!
+    # Black has won!
+    # White has won!
+    # The game is a draw...
 
+print("""
+
+● - ● - ● - ● - ●
+| \ | / | \ | / |
+● - ● - ● - ● - ●
+| / | \ | / | \ |
+● - ● -   - ○ - ○
+| \ | / | \ | / |
+○ - ○ - ○ - ○ - ○
+| / | \ | / | \ |
+○ - ○ - ○ - ○ - ○
+
+""")
