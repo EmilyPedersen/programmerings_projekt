4e6c468b93874638fd7f9949b774789befa50711
Olivia Jespersen <oliviajespersen@Olivias-MacBook-Pro.local>
2023-11-27 14:51:27 +0100

    Changed some formatting and wording

--- alquerque.py
+++ alquerque.py
@@ -26,6 +26,7 @@ def ask_for_number(minimum: int, maximum: int,
 
 def ask_for_move(b: Board) -> Move:
     """Ask the user for a legal move. Keep prompting until one is gotten."""
+    print("Get in the groove, it's time for you to make a move!")
     has_found_legal_move = False
     while not has_found_legal_move:
         source = ask_for_number(
@@ -38,6 +39,7 @@ def ask_for_move(b: Board) -> Move:
         else:
             print("That's not quite possible, we might have an issue. If you want to make it right, here are some moves you can do:")
             print_moves(b)
+    return next_move
 
 
 def print_board(b: Board) -> None:
@@ -72,17 +74,17 @@ print("\nIt's time to get quirky, let's play some Alquerque!\n")
 print("You can hear the crowd cheer, but first let's get the rules clear...\n")
 
 white_is_ai = ask_yes_or_no(
-    "Will you stand in the limelight or will you let the AI be white? (y/n)",
-    "Please just type y or n, it's all that we need. You know who'd be proud of you? Frances Shand Kydd.")
+    "Will you step away from the limelight and let the AI play white? (yes/no)",
+    "Please just type yes or no, it's all that we need. You know who'd be proud of you? Frances Shand Kydd.")
 black_is_ai = ask_yes_or_no(
-    "If you need to hit the sack, will you just let the AI play black? (y/n)",
-    "I really am feeling the rhythm and blues, a simple y or n, that's all you have to choose.")
+    "It look like you need to hit the sack, will you just let the AI play black? (yes/no)",
+    "I'm really feeling the rhythm of the blues, a simple yes or no, that's all you have to choose.")
 ai_difficulty = ask_for_number(
     1, 7,
-    "We crafted this software, it's a true piece of art, now decide, should the AI be dumb or smart? (1-7)",
-    "With that attitude you won't come into heaven, so please just pick a number between 1 and 7.")
+    "We crafted this software, it's a true piece of art, now you must decide, should the AI be dumb or smart? (1-7)",
+    "With an attitude like that you won't get into heaven, so please just pick a number between 1 and 7.")
 
-print("This is the board we'll be playing tonight. Get to know it now, so you don't lose sight.\n")
+print("This is the board we'll be playing on tonight. Get to know it now, so you don't end up losing sight.\n")
 
 print("01 - 02 - 03 - 04 - 05")
 print("|  \ |  / |  \ |  / |")
@@ -92,9 +94,9 @@ print("11 - 12 - 13 - 14 - 15")
 print("|  \ |  / |  \ |  / |")
 print("16 - 17 - 18 - 19 - 20")
 print("|  / |  \ |  / |  \ |")
-print("21 - 22 - 23 - 24 - 25")
+print("21 - 22 - 23 - 24 - 25\n")
 
-print("\nIt's time to get started, find your laminar flow. Let's hope your hands are steady. Ready, set, go!\n")
+print("It's time to get started, so find your laminar flow. Let's hope your hands are steady. Ready, set, go!\n")
 
 b = make_board()
 
@@ -102,20 +104,23 @@ while not is_game_over(b):
     print_board(b)
 
     if white_plays(b):
-        print("White's turn")
+        print("  White's turn\n")
     else:
-        print("Black's turn")
+        print("  Black's turn\n")
 
     if white_plays(b) and white_is_ai or not white_plays(b) and black_is_ai:
         move(next_move(b, ai_difficulty), b)
     else:
         move(ask_for_move(b), b)
+        print()
+
 
 print_board(b)
+print("    Game over\n")
 
 if not white(b):
     print("You fought with all your might and the game is now done, so it's a pleasure to announce that black won!")
 elif not black(b):
     print("You fought with all your might and the game is now done, so it's a pleasure to announce that white won!")
 else:
-    print("This game was as complex as the United States national law, but in the end 'twas for nothing, it ended in a draw.")
+    print("This game was as complex as the U.S. nationality law, but in the end 'twas for nothing... it ended in a draw.")
