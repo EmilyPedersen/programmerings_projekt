d6d17823f4dc8508d265d0e3d783c936f1d52ff9
Marcus Søndergaard <marcusodense@gmail.com>
2023-12-21 09:41:10 +0100

    Added outline for minimax

--- minimax.py
+++ minimax.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from board import *
 from move import *
 
@@ -7,3 +9,46 @@ def next_move(b: Board, depth: int = 3) -> Move:
     Find this by building a minimax tree with the given board and depth.
     """
     return legal_moves(b)[0]
+
+
+@dataclass
+class Tree:
+    first_child: 'Node'
+    board: Board
+    depth: int
+    value: int
+
+
+@dataclass
+class Node:
+    next_sibling: 'Node'
+    first_child: 'Node'
+    board: Board
+    move: Move
+    value: int
+
+
+def make_tree(b: Board, depth: int) -> Tree:
+    """Make a new minimax tree with the given board and depth."""
+
+
+def make_node(b: Board, depth: int) -> Node:
+    """Make a new node (and its children) from the given board and depth."""
+
+
+def rate_tree(t: Tree) -> None:
+    """Rate all the nodes in the tree."""
+
+
+def rate_node(n: Node, depth: int) -> int:
+    """Rate a node and all its children.
+    Return the value that a parent would have.
+    The depth is used to determine whether
+    the node is a min or max node.
+    """
+
+
+def rate_board(b: Board) -> int:
+    """Return a value telling how good the given board
+    is for the current player.
+    """
